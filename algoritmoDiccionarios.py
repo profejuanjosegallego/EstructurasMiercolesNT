88ae42c13db31a10b41739fa55c330143bb03284
Bernardo A Mira García <bamira1@misena.edu.co>
2026-02-11 08:43:45 -0500

    feat se crea ejemplo de diccionarios

---
+++ algoritmoDiccionarios.py
@@ -0,0 +1,25 @@
+#Manual basico para manipular diccionarios en python
+
+#1. crear un diccionario
+estudiante={}
+
+#2. poblar ("llenar") un diccionario
+estudiante["id"]=78978
+estudiante["nombre"]="Juan Jose Gallego"
+estudiante["identificacion"]="1017170698"
+estudiante["telefono"]="3225962363"
+estudiante["correo"]="correo@gmail.com"
+estudiante["promedio"]=3.5
+estudiante["esBecado"]=False
+
+#3. CICLOS EN PYTHON
+#while
+'''contador=1
+while(contador<=10):
+    print(contador)
+    contador+=1
+print("adios")'''
+#for
+for _ in range(25):
+    print("hola")
+
---
+++ algoritmoPrincipal.py
@@ -0,0 +1,18 @@
+#Definiendo variables en python
+
+#Problema:Quiero calcular el promedio de un conjunto de mediciones asociadas a un grupo de estudiantes
+
+#Datos de entrada 1: Almacenar la informacion de cada estudiante (id,numeroIdentificacion,nombres,semestre,correo,telefono,es becado?,promedio)
+id=7897
+numeroIdentificacion="1017170898"
+nombres="Juan Jose"
+semestre=3
+correo="juan@gmail.com"
+telefono="3225962363"
+esBecado=True
+promedio=3.2
+
+
+
+
+
